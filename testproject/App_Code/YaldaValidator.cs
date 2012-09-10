9e6cb8c13b394f9b4f61bf24eed03ed838092340
tahabahraminezhad <tahabahraminezhad@yahoo.com>
2012-09-10 04:53:23 +0430

    yalda validator commit

---
+++ testproject/App_Code/YaldaValidator.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace testproject
+{
+    /// <summary>
+    /// validate the file extension, size, and exists of a file in FileUpload Control
+    /// </summary>
+    public class YaldaValidator : BaseValidator
+    {
+        string _extensions = "";
+        public string Extensions
+        {
+            get { return _extensions; }
+            set { _extensions = value; }
+        }
+
+        int _sizeOfTheFileInKb = 1;
+        public int SizeOfTheFileInKb
+        {
+            get { return _sizeOfTheFileInKb; }
+            set { _sizeOfTheFileInKb = value; }
+        }
+        protected override bool EvaluateIsValid()
+        {
+            var fu = (FileUpload)FindControl(this.ControlToValidate);
+            
+            string[] ex = _extensions.Split('+');
+            if (fu.HasFile && fu.PostedFile.ContentLength <= _sizeOfTheFileInKb * 1024 && ex.Contains(System.IO.Path.GetExtension(fu.PostedFile.FileName)))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ testproject/WebForm1.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace testproject
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        
+        }        
+    }
+}
+
+
+
